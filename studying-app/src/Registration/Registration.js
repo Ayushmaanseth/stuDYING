a55d6dde14285d2a1325c677b5ea0e95c42ddf70
Ashly Lau <ashlylau@gmail.com>
2019-03-09 16:09:08 +0000

    Collected name and email data

--- studying-app/src/Registration/Registration.js
+++ studying-app/src/Registration/Registration.js
@@ -32,73 +32,86 @@ const styles = theme => ({
   },
 });
 
-const currencies = [
-  {
-    value: 'USD',
-    label: '$',
-  },
-  {
-    value: 'EUR',
-    label: '€',
-  },
-  {
-    value: 'BTC',
-    label: '฿',
-  },
-  {
-    value: 'JPY',
-    label: '¥',
-  },
-];
-
-
 class TextFields extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: '',
+                  email: '',
+                  selectedTopics: [''],};
+
+    // this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
   state = {
-    name: 'Cat in the Hat',
-    age: '',
-    multiline: 'Controlled',
-    currency: 'EUR',
+    name: 'default',
+    email: '',
+    selectedTopics: [''],
   };
 
-  handleChange = name => event => {
-    this.setState({ [name]: event.target.value });
+  // handleChange(event) {
+  //   //this.setState({name: event.target.value});
+  //   event.preventDefault();
+  //   this.setState({ name: event.target.value });
+  // };
+
+  // handleChange = name => event => {
+  //    event.preventDefault();
+  //    console.log(event.target.value);
+  //     this.setState({ [name]: event.target.value });
+  //   };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let topics = document.getElementById('topic')
+    // console.log("hello");
+    alert('submitted! with topic: ' + topics);
+
   };
 
   render() {
     const { classes } = this.props;
 
     return (
-      <form className={classes.container} noValidate autoComplete="off">
+      <form className={classes.container}>
 
-      <p class="container">
+      <p className="container">
         <img src={logo} width={300} />
       </p>
 
-      <p class="container">
+      <p className="container">
         <img src={profilepic} width={100} height={100} />
       </p>
 
         <TextField
           required
-          id="standard-password-input"
+          // <input type="text" value={this.state.value} onChange={this.handleChange} />
+          id="name"
           label="Name"
           className={classes.textField}
           type="name"
-          autoComplete="current-password"
+          // autoComplete="current-password"
           margin="normal"
         />
 
         <TextField
           required
-          id="standard-password-input"
+          id="email"
           label="Email"
           className={classes.textField}
           type="email"
-          autoComplete="current-password"
+          // autoComplete="current-password"
           margin="normal"
         />
 
-        <Downshift />
+        <Downshift
+          id='topics'
+          />
+
+        <input type="submit" onClick={this.handleSubmit} value="Submit" />
 
       </form>
     );
